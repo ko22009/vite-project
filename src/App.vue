fa0c3b017f304188ea9fe0064e83ee2fab658e11
Ruslan Kutliakhmetov <kosid2@gmail.com>
2024-09-08 23:11:52 +0500

    init project

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex justify-center">
+    <Button label="Check" icon="pi pi-check" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import 'primeicons/primeicons.css';
+import Button from 'primevue/button';
+import './main';
+</script>
